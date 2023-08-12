3199c19df6d5a3ba42d0696d652a3e2e2244db06
Dennis <10233439+idea404@users.noreply.github.com>
2023-08-12 09:59:59 +0200

    feat(contract): add contract and first tests

---
+++ contracts/TutorialSubmission.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TutorialSubmission {
+    
+    struct Submission {
+        uint256 poapNftId;
+        string deployedTestnetAddress;
+        string tutorialName;
+        string status;
+    }
+
+    // Array to store all submissions
+    Submission[] public submissions;
+
+    // Mapping from POAP NFT ID to an array of submissions
+    mapping(uint256 => Submission[]) public accountSubmissions;
+
+    // Array of authorized account addresses
+    address[] public authorizedAddresses;
+
+    // Events
+    event TutorialSubmitted(uint256 poapNftId, string deployedTestnetAddress, string tutorialName);
+    event StatusUpdated(uint256 poapNftId, string tutorialName, string newStatus);
+
+    constructor() {
+        // The contract deployer is initially an authorized address
+        authorizedAddresses.push(msg.sender);
+    }
+
+    modifier onlyAuthorized() {
+        bool isAuthorized = false;
+        for (uint256 i = 0; i < authorizedAddresses.length; i++) {
+            if (msg.sender == authorizedAddresses[i]) {
+                isAuthorized = true;
+                break;
+            }
+        }
+        require(isAuthorized, "Not authorized to call this function");
+        _;
+    }
+
+    function _assertIsNewTutorial(uint256 poapNftId, string memory tutorialName) private view {
+        for (uint256 i = 0; i < accountSubmissions[poapNftId].length; i++) {
+            Submission memory submission = accountSubmissions[poapNftId][i];
+            if (keccak256(abi.encodePacked(submission.tutorialName)) == keccak256(abi.encodePacked(tutorialName))) {
+                revert("Tutorial already submitted");
+            }
+        }
+    }
+
+    function submitTutorial(uint256 poapNftId, string memory deployedTestnetAddress, string memory tutorialName) public {
+        _assertIsNewTutorial(poapNftId, tutorialName);
+
+        Submission memory newSubmission = Submission({
+            poapNftId: poapNftId,
+            deployedTestnetAddress: deployedTestnetAddress,
+            tutorialName: tutorialName,
+            status: "PENDING"
+        });
+
+        submissions.push(newSubmission);
+        accountSubmissions[poapNftId].push(newSubmission);
+
+        emit TutorialSubmitted(poapNftId, deployedTestnetAddress, tutorialName);
+    }
+
+    function viewSubmissions() public view returns (Submission[] memory) {
+        return submissions;
+    }
+
+    function viewAccountSubmissions(uint256 poapNftId) public view returns (Submission[] memory) {
+        return accountSubmissions[poapNftId];
+    }
+
+    function updateSubmissionStatus(uint256 poapNftId, string memory tutorialName, string memory newStatus) public onlyAuthorized {
+        for (uint256 i = 0; i < submissions.length; i++) {
+            if (submissions[i].poapNftId == poapNftId && keccak256(abi.encodePacked(submissions[i].tutorialName)) == keccak256(abi.encodePacked(tutorialName))) {
+                submissions[i].status = newStatus;
+                emit StatusUpdated(poapNftId, tutorialName, newStatus);
+                break;
+            }
+        }
+    }
+
+    function addAuthorizedAddress(address newAddress) public onlyAuthorized {
+        authorizedAddresses.push(newAddress);
+    }
+
+    function removeAuthorizedAddress(address addressToRemove) public onlyAuthorized {
+        for (uint256 i = 0; i < authorizedAddresses.length; i++) {
+            if (authorizedAddresses[i] == addressToRemove) {
+                authorizedAddresses[i] = authorizedAddresses[authorizedAddresses.length - 1];
+                authorizedAddresses.pop();
+                break;
+            }
+        }
+    }
+}
---
+++ test/main.test.ts
@@ -0,0 +1,86 @@
+import { expect } from "chai";
+import { Wallet, Provider, Contract } from "zksync-web3";
+import * as hre from "hardhat";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+const RICH_WALLET_PK_1 = "0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110";
+const RICH_WALLET_PK_2 = "0xac1e735be8536c6534bb4f17f06f6afc73b2b5ba84ac2cfb12f7461b20c0bbe3";
+
+describe("TutorialSubmission", function () {
+  let tutorialContract: Contract;
+  let ownerWallet: Wallet;
+  let userWallet: Wallet;
+
+  before(async function () {
+    // deploy the contract
+    const provider = Provider.getDefaultProvider();
+    ownerWallet = new Wallet(RICH_WALLET_PK_1, provider);
+    userWallet = new Wallet(RICH_WALLET_PK_2, provider);
+    const deployer = new Deployer(hre, ownerWallet);
+    const artifact = await deployer.loadArtifact("TutorialSubmission");
+    tutorialContract = await deployer.deploy(artifact, []);
+  });
+
+  it("Should get default empty array of submissions", async function () {
+    let expected: any = [];
+    const first = await tutorialContract.viewSubmissions();
+    expect(first).to.deep.equal(expected);
+  });
+
+  it("Should have user create new submission, add it and return new array", async function () {
+    const submission = {
+      poapNftId: 1,
+      deployedTestnetAddress: "0x0000000000",
+      tutorialName: "Tutorial 1",
+    };
+    const userTutorialContract = new Contract(tutorialContract.address, tutorialContract.interface, userWallet);
+    const addSubmissionTx = await userTutorialContract.submitTutorial(submission.poapNftId, submission.deployedTestnetAddress, submission.tutorialName);
+    await addSubmissionTx.wait();
+    let expected = [{ ...submission, status: "PENDING" }];
+    const secondRaw = await tutorialContract.viewSubmissions();
+    const second = secondRaw.map(convertToSubmissionObject);
+    expect(second).to.deep.equal(expected);
+  });
+
+  it("Should update existing submission and return new array", async function () {
+    const submission = {
+      poapNftId: 1,
+      deployedTestnetAddress: "0x0000000000",
+      tutorialName: "Tutorial 1",
+      status: "APPROVED",
+    };
+    const setStatusTx = await tutorialContract.updateSubmissionStatus(submission.poapNftId, submission.tutorialName, submission.status);
+    // wait until the transaction is mined
+    await setStatusTx.wait();
+    let expected = [{ ...submission }];
+    const secondRaw = await tutorialContract.viewSubmissions();
+    const second = secondRaw.map(convertToSubmissionObject);
+    expect(second).to.deep.equal(expected);
+  });
+
+  it("Should fail when user tries to update a submission", async function () {
+    const submission = {
+      poapNftId: 1,
+      deployedTestnetAddress: "0x0000000000",
+      tutorialName: "Tutorial 1",
+      status: "APPROVED",
+    };
+    const userTutorialContract = new Contract(tutorialContract.address, tutorialContract.interface, userWallet);
+    try {
+      const setStatusTx = await userTutorialContract.updateSubmissionStatus(submission.poapNftId, submission.tutorialName, submission.status);
+      await setStatusTx.wait();
+      expect.fail("Expected updateSubmissionStatus to revert, but it didn't");
+    } catch (error) {
+      expect(error.message).to.include("execution reverted: Not authorized to call this function");
+    }
+  });
+});
+
+function convertToSubmissionObject(arr: any[]): any {
+  return {
+    poapNftId: arr[0].toNumber(), // Convert BigNumber to number
+    deployedTestnetAddress: arr[1],
+    tutorialName: arr[2],
+    status: arr[3],
+  };
+}
