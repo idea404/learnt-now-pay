559d7af61eb10bface0ae97ec4af230748c7eb78
Dennis <10233439+idea404@users.noreply.github.com>
2023-10-03 20:07:51 +0200

    fix: test provider init

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ import "@matterlabs/hardhat-zksync-solc";
 import "@matterlabs/hardhat-zksync-verify";
 
 // dynamically changes endpoints for local tests
-const zkSyncTestnet =
+export const zkSyncTestnet =
   process.env.NODE_ENV == "test"
     ? {
         url: "http://127.0.0.1:8011",
